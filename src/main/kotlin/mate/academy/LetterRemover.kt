3507849d6149b0ab59371e380de61f9d9dca3752
Dmytro Sokolovskyi <dm.sokolovskyi@gmail.com>
2025-08-22 00:16:10 -0400

    fix build failure

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,9 +1,8 @@
 package mate.academy
 
-class LetterRemover {
-    companion object {
-        @JvmStatic fun removeLetter(text: String): CharArray {
-            return text.lowercase().replace("r", "!").toCharArray()
-        }
+object LetterRemover {
+    @JvmStatic
+    fun removeLetter(text: String): CharArray {
+        return text.lowercase().replace("r", "!").toCharArray()
     }
 }
